bc83fee275b0321e34bdc49ea39ec8b71f0171b5
ZUCKLEVENTE-PC\ASUS <zuck.levente@protonmail.com>
2018-11-22 14:56:59 +0100

    Some bugfix.

--- CSHARP/oenik_prog3_2018_2_bbh0e5/VideoSharing.Repository/VideoRepository.cs
+++ CSHARP/oenik_prog3_2018_2_bbh0e5/VideoSharing.Repository/VideoRepository.cs
@@ -6,6 +6,7 @@ namespace VideoSharing.Repository
 {
     using System;
     using System.Collections.Generic;
+    using System.Data.Entity;
     using System.Linq;
     using System.Text;
     using System.Threading.Tasks;
